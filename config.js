e812f962f23a7ba67a03a3f9536c90f2d8a2f8ea
tonyau2 <tonyau2_@hotmail.com>
2026-07-18 07:34:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.CATA = {
+
+  webapp: "https://script.google.com/macros/s/AKfycbxoO0_UI-WbU8d5cRakn9RXU0ofeBqjbPST1ODfn_oDqJ__wHidzRxCPA-B1A-iq8Q3/exec",
+
+  logo: "logo-trim.png",
+
+  fbPage: ""
+
+};
